6b9042665f9de3de3dedc78870f5df7674e4e825
Camilly Reis <millyvictoria.2011@gmail.com>
2023-03-15 10:51:14 -0300

    Segunda atividade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ConteudoComponent } from './conteudo/conteudo.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ConteudoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
